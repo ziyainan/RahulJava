404899c96937ee6c09074f9bf1df1f6bef58bd73
ziyainan <inanzya@gmail.com>
2020-08-11 20:50:01 -0400

    Nuran ogretti

---
+++ src/Collections/a.java
@@ -0,0 +1,33 @@
+package Collections;
+
+import java.util.*;
+
+public class a {
+
+    public static void main(String[] args) {
+
+
+        ArrayList list = new ArrayList();
+
+        list.add(5);
+        list.add("ziya");
+
+        System.out.println(list);
+        System.out.println(list.get(1));
+
+       // HashMap hm=new HashMap();
+
+        HashMap <Integer,String> hm= new HashMap<Integer, String>();
+        hm.put(1,"a");
+        System.out.println(hm.get(1));
+
+
+
+
+
+    }
+
+
+
+
+}
